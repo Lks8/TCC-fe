b5c8c2682f9dee322aec7a759d75265eddfcd037
Lkns8 <lucoelho@uolinc.com>
2021-08-23 19:04:10 -0300

    Tela de Login

--- tcc-fe/src/App.vue
+++ tcc-fe/src/App.vue
@@ -21,7 +21,12 @@ export default {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+    background-color: #252829;
+    height: 923px;
+    margin: 0;
+}
+
+.div {
+    margin: 0;
 }
 </style>
--- tcc-fe/src/components/Login.vue
+++ tcc-fe/src/components/Login.vue
@@ -1,11 +1,17 @@
 <template>
-    <div>
-        <container>
-            <img src="C:\Meu\TCC-fe\tcc-fe\src\assets\logo.png">
-            <p>
-                Mama aqui o glub glub
-            </p>
-        </container>
+    <div class="main">
+        <div class="core">
+            <div class="logo">
+                <img src="/assets/logo.png" alt="logo" />
+            </div>
+            <div class="login">
+                <p>Login</p>
+                <input />
+                <p>Senha</p>
+                <input />
+                <b-button> Entrar </b-button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -16,4 +22,40 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.main {
+    display: inline-flex;
+    height: 500px;
+}
+
+.core {
+    width: 400px;
+}
+
+.logo {
+    height: 120px;
+    display: inline-block;
+    background-color: #d2d3d4;
+    width: 400px;
+}
+
+.login {
+    padding: 10px;
+    background-color: white;
+}
+
+p {
+    text-align: left;
+}
+
+input {
+    display: flex;
+    width: 350px;
+    height: 30px;
+}
+
+b-button {
+    color: red;
+    background-color: blue;
+}
+</style>
