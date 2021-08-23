7433ed9c7d596f1036237427a52c6f77635ade3e
Lks8 <lucas.martins.coelho@hotmail.com>
2021-08-23 11:29:30 -0300

    Config pronta

--- tcc-fe/src/App.vue
+++ tcc-fe/src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <Login />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from "./components/Login.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        Login,
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ tcc-fe/src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <container>
+            <p>
+                Mama aqui o glub glub
+            </p>
+        </container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    props: {},
+};
+</script>
+
+<style scoped></style>
