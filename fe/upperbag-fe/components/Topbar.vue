4daab7534bff97d288e66341eb55552f07fdcd11
Lks8 <lucas.martins.coelho@hotmail.com>
2021-08-31 11:33:12 -0300

    redooooo

---
+++ fe/upperbag-fe/components/Topbar.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="topbar-core">
+        <b-button to="/">
+            <img
+                class="logowhite"
+                src="/assets/logo_branco.png"
+                alt="logo"
+                to="/"
+        /></b-button>
+        <div class="user">
+            <h1 class="username">Jacinto Pinto Aquino Rego</h1>
+            <img class="userpic" src="/assets/user_pic.jpg" alt="userpic" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Rubik&display=swap");
+.topbar-core {
+    display: flex;
+    justify-content: space-between;
+    background: rgb(53, 57, 59);
+    background: linear-gradient(
+        0deg,
+        rgba(24, 26, 27, 1) 0%,
+        rgba(53, 57, 59, 1) 100%
+    );
+    margin-bottom: 100px;
+}
+
+.logowhite {
+    display: flex;
+    height: 60px;
+    margin: 10px;
+}
+
+.user {
+    display: flex;
+}
+
+.username {
+    font-family: "Rubik";
+    font-weight: normal;
+    color: #d2d3d4;
+    align-self: center;
+    font-size: 22px;
+}
+
+.userpic {
+    margin-right: 50px;
+    margin-left: 20px;
+    height: 50px;
+    align-self: center;
+    border-radius: 40px;
+    border: white;
+}
+</style>
---
+++ fe/upperbag-fe/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Login />
+    <Dashboard />
+  </div>
+</template>
+
+<script>
+import Login from "/pages/Login.vue";
+import Dashboard from "/pages/Dashboard.vue";
+export default {
+  components: {
+    Login,
+    Dashboard
+  }
+};
+</script>
