e8f09003f75a662167f8285a40f42b7e4d16ef7a
Lks8 <lucas.martins.coelho@hotmail.com>
2021-09-09 21:49:30 -0300

    sidebar

--- upperbag-fe/components/Sidebar.vue
+++ upperbag-fe/components/Sidebar.vue
@@ -1,28 +1,29 @@
-<template> 
-    <div>
-        <div class="sidebar-core col-12">
-            <b-row>
-                <p class="button-dash">Dashboard</p>
-            </b-row>
-            <b-row>
-                <p class="button-user-list">User Control List</p>
-            </b-row>
-        </div>
-    </div>
+<template>
+	<div>
+		<div class="sidebar-core col-12">
+			<p class="button-dash">Dashboard</p>
+
+			<p class="button-user-list">User Control List</p>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {};
+	export default {};
 </script>
 
 <style>
-.sidebar-core {
-    color: #d2d3d4;
-    display: flex;
-    align-items: flex-start;
-    flex-direction: column;
-    background: rgb(53,57,59);
-    background: linear-gradient(0deg, rgba(24,26,27,1) 0%, rgba(53,57,59,1) 100%);
-    height: 100%;
-}
+	.sidebar-core {
+		color: #d2d3d4;
+		display: flex;
+		align-items: flex-start;
+		flex-direction: column;
+		background: rgb(53, 57, 59);
+		background: linear-gradient(
+			0deg,
+			rgba(24, 26, 27, 1) 0%,
+			rgba(53, 57, 59, 1) 100%
+		);
+		height: 100%;
+	}
 </style>
--- upperbag-fe/pages/Dashboard.vue
+++ upperbag-fe/pages/Dashboard.vue
@@ -1,73 +1,81 @@
 <template>
-    <div>
-        <header class="topbar"><Topbar /></header>
-        <body class="body">
-            <b-container class="graph-main">
-                <Graph />
-            </b-container>
-            <b-row>
-                <b-col class="filters-main">
-                    <Filters />
-                </b-col>
-                <b-col class="table-main">
-                    <TableDashboard />
-                </b-col>
-            </b-row>
-        </body>
-        <div class="sidebar" v-show="false"><Sidebar /></div>
-    </div>
+	<div>
+		<header class="topbar"><Topbar /></header>
+		<div class="sidebar">
+			<Sidebar />
+		</div>
+		<body class="body">
+			<b-container class="graph-main">
+				<Graph />
+			</b-container>
+			<b-row>
+				<b-col class="filters-main">
+					<Filters />
+				</b-col>
+				<b-col class="table-main">
+					<TableDashboard />
+				</b-col>
+			</b-row>
+		</body>
+	</div>
 </template>
 
 <script>
-import Sidebar from "../components/Sidebar.vue";
-import Topbar from "../components/Topbar.vue";
-import Filters from "../components/Dashboard/Filters.vue";
-import Graph from "../components/Dashboard/Graph.vue";
-import TableDashboard from "../components/Dashboard/TableDashboard.vue";
-export default {
-    components: { Topbar, Sidebar, Filters, Graph, TableDashboard },
-};
+	import Sidebar from "../components/Sidebar.vue";
+	import Topbar from "../components/Topbar.vue";
+	import Filters from "../components/Dashboard/Filters.vue";
+	import Graph from "../components/Dashboard/Graph.vue";
+	import TableDashboard from "../components/Dashboard/TableDashboard.vue";
+	export default {
+		components: { Topbar, Sidebar, Filters, Graph, TableDashboard },
+	};
 </script>
 
 <style>
-.body {
-    background-color: crimson;
-    width: 72%;
-    margin: 0 auto;
-    height: 100%;
-}
+	.body {
+		background-color: crimson;
+		width: 72%;
+		margin: 0 auto;
+		height: 100%;
+	}
 
-.topbar {
-    height: 80px;
-    width: 100%;
-}
+	.topbar {
+		height: 80px;
+		width: 100%;
+	}
 
-.sidebar {
-    height: 100%;
-}
+	.sidebar {
+		height: 100%;
+		width: 160px;
+		position: fixed;
+		z-index: 1;
+		top: 80px;
+		left: 0;
+		background-color: #111;
+		overflow-x: hidden;
+	}
 
-.graph-main {
-    display: flex;
-    height: 250px;
-    text-align: center;
-    align-items: center;
-    justify-content: center;
-}
+	.graph-main {
+		display: flex;
+		height: 250px;
+		text-align: center;
+		align-items: center;
+		justify-content: center;
+	}
 
-.table-main {
-    height: 200px;
-    display: flex;
-    text-align: center;
-    align-items: center;
-    justify-content: center
-}
+	.table-main {
+		height: 200px;
+		display: flex;
+		text-align: center;
+		align-items: center;
+		justify-content: center;
+	}
 
-.filters-main {
-    height: 200px;
-    display: flex;
-    text-align: center;
-    align-items: center;
-    justify-content: center
-}
-
+	.filters-main {
+		height: 200px;
+		display: flex;
+		text-align: center;
+		align-items: center;
+		justify-content: center;
+	}
 </style>
